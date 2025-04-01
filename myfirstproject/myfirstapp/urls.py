b8d86e9369677528f99445125d916561164e6674
Yoann_glitch <yoyo.mul007@gmail.com>
2025-04-01 09:10:08 +0200

    R209

--- myfirstproject/myfirstapp/urls.py
+++ myfirstproject/myfirstapp/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
- path('index/', views.index),
-    path('formulaire/', views.formulaire),
-    path('bonjour/', views.bonjour),
- ]
+path('index/', views.index),
+]
