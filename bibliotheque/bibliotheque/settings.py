7f2de33136d6139fc2e4fabfa726dc9940c5f44c
Yoann_glitch <yoyo.mul007@gmail.com>
2025-04-03 14:37:19 +0200

    R209

--- bibliotheque/bibliotheque/settings.py
+++ bibliotheque/bibliotheque/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bibliotheque.app.apps.Bibliotheque.appConfig'
+    'bibliothequeapp.apps.BibliothequeappConfig'
 ]
 
 MIDDLEWARE = [
