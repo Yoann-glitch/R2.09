64282508f6fa766413f1e0fb64c1691bb1f990fc
Yoann_glitch <yoyo.mul007@gmail.com>
2025-04-03 14:33:42 +0200

    R209

--- bibliotheque/bibliotheque/urls.py
+++ bibliotheque/bibliotheque/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for projetTP2 project.
+URL configuration for bibliotheque project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/dev/topics/http/urls/
