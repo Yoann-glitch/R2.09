1ba3841b52ee31df466a70d885e290593fb19173
Yoann_glitch <yoyo.mul007@gmail.com>
2025-04-03 15:36:31 +0200

    R209

--- bibliotheque/bibliotheque/urls.py
+++ bibliotheque/bibliotheque/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from bibliotheque import views
+from . import views
 
 urlpatterns = [
     path('ajout/', views.ajout),
