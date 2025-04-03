fc48ea1cc5ee025d397317357753fe4d90d20d0a
Yoann_glitch <yoyo.mul007@gmail.com>
2025-04-03 15:37:10 +0200

    R209

--- bibliotheque/bibliotheque/urls.py
+++ bibliotheque/bibliotheque/urls.py
@@ -19,6 +19,8 @@ from . import views
 
 urlpatterns = [
     path('ajout/', views.ajout),
+    path('traitement', views.traitement), # ajouter la route traitement associé à l'action traitement du fichier views.py
+
 
 
 ]
